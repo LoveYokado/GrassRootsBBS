ffce143b857dc4e02da1e4af65520fa7fc305584
midyuki <midyuki@protonmail.com>
2025-10-07 17:28:56 +0900

    こんなもんだろ！

--- static/js/admin.js
+++ static/js/admin.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const sidebarToggle = document.getElementById('sidebar-toggle');
     if (sidebarToggle) {
         sidebarToggle.addEventListener('click', function () {
-            document.body.classList.toggle('sidebar-closed');
+            document.body.classList.toggle('sidebar-open');
         });
     }
 });
